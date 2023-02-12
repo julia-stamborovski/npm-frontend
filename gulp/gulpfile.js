c5212107968779495f2f2f9539731d712aee8c36
Julia Stamborovski <juliashizuko66@gmail.com>
2023-02-12 12:22:47 -0300

    Gulp Autoprefixer
    Substituir a opção 'browsers' do Autoprefixer com a configuração do Browserlist. -Utilize a key 'browserlist' no arquivo package.json ou no arquivo .browserslistrc.; - a opção 'browsers' não é mais recomendada e pode causar erros.; - se necessário, a opção 'browsers' pode ser renomeada para: 'overrideBrowserslist'; -adotar a configuração do Browserlist fornece uma única configuração para várias ferramentas, como Babel, Autoprefixer, postcss-normalize, e entre outros.; - para obter mais infos, visite:  https://github.com/browserslist/browserslist#readme e https://twitter.com/browserslist; **nota:** Esse 'projeto' foi feito com fins de aprendizado e não foi implementada essa mudança no projeto. Mas caso queira, implementar em um projeto para uso real, acima deixo as configurações corretas. O objetivo aqui é apenas entender como utilizar npm para automação frontend.

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,10 +1,18 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+
 function compilaSass()
 {
     return gulp
         .src('css/scss/*.scss')
-        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(autoprefixer ({
+                browsers: ["last 2 versions"],
+                cascade: false
+        }))
         .pipe(gulp.dest('css/'))
 }
 
